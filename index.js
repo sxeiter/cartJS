b3f2169c421ad582aa1b8f41858c5b26010d2044
Павел Бакшеев <sxeiter@mail.ru>
2023-10-11 16:12:35 +0500

    дз 9

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 'use strict';
 
 const cart = {
-    items: [],
-    totalPrice: 0,
-    count: 0,
-    getTotalPrice () {
-        return this.totalPrice;
-    },
-    add (nameItem, price ,sum = 1) {
-        const newItem = {
-            nameItem,
-            price,
-            sum,
-        }
-        this.count += 1; 
-        this.items.push(newItem);
-        this.calculateItemPrice();
-    },
-    increaseCount (number) {
-        this.count += number;
-    },
-    calculateItemPrice () {
-        this.totalPrice = 0;
-        this.items.forEach((item) => {
-            this.totalPrice += item.price * item.sum;
-    })
-    },
-    clear () {
-        this.items = [];
-        this.totalPrice = 0;
-        this.count = 0;
-    },
-    print () {
-        const cartStr = JSON.stringify(cart); 
-        console.log(cartStr);
-        console.log("Общая стоимость корзины:" ,this.getTotalPrice());
-    }
-}
+  items: [],
+  get totalPricing() {
+    this.totalPrice = this.calculateItemPrice();
+  },
+  totalPrice: 0,
+  count: 0,
+  add(nameItem, price, sum = 1) {
+    const newItem = {
+      nameItem,
+      price,
+      sum,
+    };
+    this.count += 1;
+    this.items.push(newItem);
+  },
+  increaseCount(number) {
+    this.count += number;
+  },
+  calculateItemPrice(val = 0) {
+    this.items.forEach((item) => {
+      val += item.price * item.sum;
+    });
+    return val;
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    const cartStr = JSON.stringify(cart);
+    console.log(cartStr);
+    console.log("Общая стоимость корзины:", this.totalPrice);
+  },
+};
+
 
 cart.clear();
 
